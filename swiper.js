749f0aa2f4b8040000a37a23da17dcdab2be6486
Eduardo Marquez <emarquez@sizengs.com>
2020-04-28 18:19:14 -0600

    Slider-Contact
    El slider ahora funciona de forma automatica, se agrego un footer que funcionara como seccion de contacto

--- swiper.js
+++ swiper.js
@@ -1,7 +1,10 @@
-var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
+    var swiper = new Swiper('.swiper-container', {
       spaceBetween: 30,
-      loop: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
